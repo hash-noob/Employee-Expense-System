2c50b70faba05d6967aeb62b7f1310c100d41afa
hash-noob <130783735+hash-noob@users.noreply.github.com>
2024-05-20 17:17:02 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-JFLASJLF
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const db = mongoose.connect("mongodb+srv://koushik110541:mongodb123@mydatabase.gzrfjum.mongodb.net/?retryWrites=true&w=majority&appName=MyDatabase").then(()=> console.log("DB connected successfully."),(err)=>{console.log(err)})
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+
+// app.get('/',async (req,res)=>{
+//     let result = await student.create(req.body);
+//     res.json(result);
+// })
+
+app.listen(port,()=>{
+    console.log(`The server is listening at ${port}`)
+});
