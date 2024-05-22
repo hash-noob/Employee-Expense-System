43fda9879d162bfcd89ebca1c4b071a141c9e24b
sanjithaa <padurisanjithagrace@gmail.com>
2024-05-22 15:27:02 +0530

    update

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,1 +1,11 @@
-SDF
+const mongoose=require(`mongoose`)
+const usersSchema=mongoose.Schema({
+    email:String,
+    password:String,
+    username:String,
+    mobileNumber:String,
+    active:Boolean,
+    role:String
+})
+const usermodel=mongoose.model("testjp",usersschema)
+module.exports=usermodel
