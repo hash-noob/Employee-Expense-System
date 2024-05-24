c3ecae2b7c30e3801d8635dfe3fd6b7116d8ffb9
sanjitha <padurisanjithagrace@gmail.com>
2024-05-24 10:27:24 +0530

    model added

--- EmployeeExpense/Employee-App/Models/login.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require("mongoose")
-const loginSchema = mongoose.Schema({
-    email:String,
-    password:String
-})
-
-const loginModel = mongoose.model("login",loginSchema)
-
-module.exports= loginModel
