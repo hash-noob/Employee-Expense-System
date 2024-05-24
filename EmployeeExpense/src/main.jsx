463c4b2d035ee3ab1e3e0e0d13475a1a43e932d8
shruthilaya <kammarishruthilaya@gmail.com>
2024-05-24 22:55:53 +0530

    navigating from login to dashboard

--- EmployeeExpense/src/main.jsx
+++ EmployeeExpense/src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './login.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import axios from 'axios'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // Corrected import statement
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>  
+   
     <App />
-  </BrowserRouter>
-)
-
+ 
+);
