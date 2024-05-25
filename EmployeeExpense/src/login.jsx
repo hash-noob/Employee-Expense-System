213c2c9ac3f938bb67c83a08b72a26f483d5e25c
shruthi <kammarishruthilaya@gmail.com>
2024-05-25 11:13:32 +0530

    test

--- EmployeeExpense/src/login.jsx
+++ EmployeeExpense/src/login.jsx
@@ -1,58 +1,62 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './App.css';
 
 function App() {
   const handleLogin = async (e) => {
     e.preventDefault();
     
     const formData = new FormData(e.target);
-    const eid = formData.get('eid');
-    const password = formData.get('password');
+    const eid = document.getElementById("eid").value;
+    const password = document.getElementById("password").value;
     const messageElement = document.getElementById('message');
 
     try {
-      const response = await axios.post('/login', { eid, password });
-
+      const response = await axios.post('http://localhost:3001/api/user/login', { eid, password });
+      console.log(response)
       if (response.status === 200) {
         // Store the token in local storage or any state management
         localStorage.setItem('token', response.data.token);
         messageElement.textContent = 'Login successful!';
       }
     } catch (error) {
-      if (error.response) {
-        // Server responded with a status other than 200 range
-        messageElement.textContent = error.response.data;
-      } else {
-        // Something else happened while setting up the request
-        messageElement.textContent = 'An error occurred. Please try again.';
-      }
+       console.log(error)
     }
   };
 
   return (
-    <>
     <div className='app-container'>
       <div className="form-container">
         <h2>Login</h2>
+        <p id="message"></p>
         <form onSubmit={handleLogin}>
-          <div className="form-group">  
-            <input type="text" placeholder="EmployeeId" name='eid' required />
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="EmployeeId"
+              name="eid"
+              id="eid"
+              required
+            />
           </div>
           <div className="form-group">
             <label>Password:</label>
-            <input type="password"  name='password' required />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+            />
           </div>
           <button type="submit" className="btn">Login</button>
           <p>
             Don't have an account? <Link to="/signup">Sign up</Link>
           </p>
         </form>
-        <p id="message"></p>
       </div>
     </div>
-    </>
   );
-};
+}
 
 export default App;
