c7cf8d4ff6b37f76255a430a41b08774cda395cc
sanjitha <padurisanjithagrace@gmail.com>
2024-07-03 11:22:40 +0530

    history

--- EmployeeExpense/src/components/manager/ClaimDetails.jsx
+++ EmployeeExpense/src/components/manager/ClaimDetails.jsx
@@ -5,14 +5,14 @@ import BillContainer from './BillContainer';
 
 const ClaimCard = ({ claim, onApprove, onReject }) => {
   return (
-    <div className="claim-card bg-white p-4 rounded-lg shadow-md">
-      <h2 className="text-2xl font-semibold mb-4">Claim Information</h2>
+    <div className="claim-card h-screen bg-white p-8 rounded-lg shadow-md flex flex-col">
+      <h2 className="text-2xl font-semibold mb-8">Claim Information</h2>
       {/* <p><strong>Claim ID:</strong> {claim.cId}</p> */}
-      <p><strong>Title:</strong> {claim.title}</p>
-      <p><strong>Total Amount:</strong> ${claim.totalAmount}</p>
-      <p><strong>Employee ID:</strong> {claim.eId}</p>
-      <p><strong>From Date:</strong> {new Date(claim.fromDate).toLocaleDateString()}</p>
-      <p><strong>To Date:</strong> {new Date(claim.toDate).toLocaleDateString()}</p>
+      <p class="mb-3"><strong>Title:</strong> {claim.title}</p>
+      <p class="mb-3"><strong>Total Amount:</strong> ${claim.totalAmount}</p>
+      <p class="mb-3"><strong>Employee ID:</strong> {claim.eId}</p>
+      <p class="mb-3"><strong>From Date:</strong> {new Date(claim.fromDate).toLocaleDateString()}</p>
+      <p class="mb-3"><strong>To Date:</strong> {new Date(claim.toDate).toLocaleDateString()}</p>
       <div className="flex justify-between mt-4">
   <button className="bg-blue-500 text-white py-2 px-4 rounded" onClick={() => onApprove(claim)}>Approve</button>
   <button className="bg-blue-600 text-white py-2 px-4 rounded" onClick={() => onReject(claim)}>Reject</button>
@@ -77,11 +77,11 @@ const ClaimDetails = ({claim}) => {
   };
 
   return (
-    <div className="claim-details grid grid-cols-1 lg:grid-cols-2 gap-4">
+    <div className="claim-details min-h-screen grid grid-cols-1 lg:grid-cols-2 gap-4">
       <div className="bills-section">
         <BillContainer bills={bills} setBills={setBills} />
       </div>
-      <div className="claim-info-section">
+      <div className="claim-info-section min-h-screen">
         <ClaimCard claim={claim} onApprove={handleApprove} onReject={handleReject} />
       </div>
     </div>
